0615e008653c663589f82f850679616a138ff3b2
Esteban Borai <estebanborai@gmail.com>
2021-02-17 20:53:12 -0300

    feat: simulate slow request response

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
@@ -8,8 +10,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_code, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
